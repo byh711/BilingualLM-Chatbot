dfdd0bb38978ea51d699a5e480de46c8ed245444
Youngho Bae <byh711@hanyang.ac.kr>
2023-09-25 23:00:58 +0900

    Add files via upload

---
+++ setting/Working_code/asr.py
@@ -0,0 +1,190 @@
+# -*- coding:utf-8 -*-
+'''
+ASR (Automatic Speech Recognition) with Microsoft Azure SDK for Python.
+This script listens to another player's speech and responds based on keyword files located in the data folder.
+'''
+
+import sys
+import keyboard
+import azure.cognitiveservices.speech as speechsdk
+import csv
+import time
+import datetime
+import numpy as np
+import onnxruntime
+from transformers import AutoTokenizer
+import warnings
+from sentence_transformers import SentenceTransformer, util
+
+from Working_code import config as cf
+from Working_code import tts
+
+# Suppress warnings
+warnings.filterwarnings('ignore')
+onnxruntime.set_default_logger_severity(3)
+
+# Configuration settings
+speech_key, service_region = cf.speech_key, cf.service_region
+speech_config = cf.speech_config
+audio_config = cf.audio_config
+logger = cf.logging.getLogger("__asr__")
+
+# Tokenizer and ONNX session based on language
+if cf.THIS_LANGUAGE == 'en-US':
+    onnx_session = onnxruntime.InferenceSession('Working_code/model/onnx_model/dialoGPT_ENG/decoder_model.onnx')
+    tokenizer = AutoTokenizer.from_pretrained('Working_code/model/onnx_model/dialoGPT_ENG')
+else:
+    onnx_session = onnxruntime.InferenceSession('Working_code/model/onnx_model/kogpt2_KOR/decoder_model.onnx')
+    tokenizer = AutoTokenizer.from_pretrained('Working_code/model/onnx_model/kogpt2_KOR', bos_token='</s>', eos_token='</s>', pad_token='<pad>')
+    
+#Load the model
+model = SentenceTransformer('sentence-transformers/multi-qa-mpnet-base-cos-v1')
+
+def asr_tts_excel():
+    mode = "w" if cf.script_counter == 1 else "a"
+    with open(cf.script_path, encoding="utf-8-sig", newline='', mode=mode) as f:
+        writer = csv.writer(f)
+        if cf.script_counter == 1:
+            writer.writerow(['Speaker', 'Script', 'Start', 'Finish', 'Self_ASR', 'Sim_score'])
+            cf.script_counter += 1
+        writer.writerow(cf.list_utter)
+
+
+class listen_micr:
+
+    def __init__(self):
+        self._running = True
+
+    def terminate(self):
+        self._running = False
+
+    def run(self):
+        speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, language=cf.THIS_LANGUAGE, audio_config=audio_config)
+
+        while self._running:
+            try:
+                print("Say Something!")
+                result = speech_recognizer.recognize_once_async().get()
+                user_utt = result.text[:-1].lower()
+
+                if user_utt:
+                    self.handle_user_input(user_utt)
+                    if ('exit' in user_utt) or ('종료' in user_utt) or (keyboard.is_pressed('esc')):
+                        print("Exiting...")
+                        sys.exit()
+
+            except Exception as e:
+                logger.error(e)
+
+    def handle_user_input(self, user_utt):
+        cf.utt_start_time = round(time.time() - cf.game_start_time, 5)
+        print("Recognized:", user_utt)
+        logger.info(f'user said: {user_utt}')
+        self.respond_to_user_utt(user_utt, cf.language)
+
+        cf.list_utter.extend(["Player", user_utt, str(datetime.timedelta(seconds=cf.utt_start_time)).split(".")[0]])
+        cf.utt_finish_time = round(time.time() - cf.game_start_time, 5)
+        cf.list_utter.append(str(datetime.timedelta(seconds=cf.utt_finish_time)).split(".")[0])
+        cf.list_utter.append('')
+        cf.list_utter.append('')
+        asr_tts_excel()
+        cf.list_utter = []
+
+    def respond_to_user_utt(self, user_text, language):
+        try:
+            decoded_text = self.get_response_text(user_text)
+            
+            # Encode query and documents
+            query_emb = model.encode(user_text, show_progress_bar=False)
+            response_emb = model.encode(decoded_text, show_progress_bar=False)
+            
+            #Compute dot score between query and all document embeddings
+            cf.sim_score = util.dot_score(query_emb, response_emb)[0].tolist()[0]
+            
+            if cf.sim_score > cf.threshold:
+                tts.synthesize_utt(decoded_text, 'asr')
+            else:
+                cf.list_utter.extend(["Avatar", decoded_text, str(datetime.timedelta(seconds=cf.utt_start_time)).split(".")[0]])
+                cf.utt_finish_time = round(time.time() - cf.game_start_time, 5)
+                cf.list_utter.append(str(datetime.timedelta(seconds=cf.utt_finish_time)).split(".")[0])
+                
+                cf.list_utter.append('under threshold')
+                cf.list_utter.append(cf.sim_score)
+                asr_tts_excel()
+                cf.list_utter = []
+                
+        except Exception as err:
+            logger.error("user input parsing failed", err)
+
+    def get_response_text(self, user_text):
+        if cf.THIS_LANGUAGE == 'en-US':
+            # Note that we use the user text directly, not wrapped in <usr> or <sys> tags, 
+            # as GPT2 doesn't use them. The eos_token is used to mark the end of the question.
+            input_ids = tokenizer.encode(user_text + tokenizer.eos_token, return_tensors='np').astype(np.int64)
+        
+            # Create attention mask
+            attention_mask = np.ones_like(input_ids, dtype=np.int64)
+        
+            decoded_input_ids = top_p_sampling_decode(input_ids, attention_mask)
+            decoded_text = tokenizer.decode(decoded_input_ids[0])
+        
+            # Strip the question text and the eos_token from the response
+            decoded_text = decoded_text[len(user_text)+len(tokenizer.eos_token):]
+            return decoded_text
+
+        else:
+            sent = '<usr>' + user_text + '<sys>'
+            input_ids = [tokenizer.bos_token_id] + tokenizer.encode(sent)
+            input_ids = np.array([input_ids], dtype=np.int64)
+        
+            # Create attention mask
+            attention_mask = np.ones_like(input_ids, dtype=np.int64)
+        
+            decoded_input_ids = top_p_sampling_decode(input_ids, attention_mask)
+            decoded_text = tokenizer.decode(decoded_input_ids[0])
+            decoded_text = decoded_text.split('<sys> ')[1].replace('</s>', '')
+            return decoded_text
+
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=-1, keepdims=True)
+
+def top_p_sampling(logits, p=0.9):
+    sorted_logits = np.sort(logits)[::-1]
+    sorted_indices = np.argsort(logits)[::-1]
+
+    cumulative_probs = np.cumsum(softmax(sorted_logits))
+
+    indices_to_remove = cumulative_probs > p
+    indices_to_remove = np.roll(indices_to_remove, 1)  # Shift the indices_to_remove by one to the right to keep the top token
+    indices_to_remove[0] = False  # Set the first token as False to always keep it
+
+    logits[sorted_indices[indices_to_remove]] = -np.inf
+    probs = softmax(logits)
+
+    token_id = np.random.choice(len(logits), size=1, p=probs)[0]
+
+    return token_id
+
+def top_p_sampling_decode(input_ids, attention_mask, max_length=50, p=0.9):
+    current_length = input_ids.shape[1]
+
+    while current_length < max_length:
+        outputs = onnx_session.run(output_names=['logits'], input_feed={"input_ids": input_ids, "attention_mask": attention_mask})
+        logits = outputs[0][0, -1, :]
+
+        # Apply Top-p sampling
+        predicted_token_id = top_p_sampling(logits, p=p)
+
+        # Break if the EOS token is generated
+        if predicted_token_id == tokenizer.eos_token_id:
+            break
+
+        # Update input_ids and attention_mask
+        input_ids = np.concatenate((input_ids, np.array([[predicted_token_id]])), axis=1).astype(np.int64)
+        attention_mask = np.concatenate((attention_mask, np.array([[1]])), axis=1).astype(np.int64)
+
+        current_length += 1
+
+    return input_ids
---
+++ setting/Working_code/state_changed.py
@@ -0,0 +1,147 @@
+# -*- coding:utf-8 -*-
+'''
+This module checks columns of the 'test.csv' file, extracts the necessary information,
+and sends appropriate data to the 'parse_tree.py' file for further processing.
+'''
+
+import time
+import traceback
+from Working_code import config as cf
+from Working_code import parse_tree as pt
+
+# Set logger
+logger = cf.logging.getLogger("__state_checker__")
+
+def get_line_from_csv(csvfile):
+    """Continuously read the current line of the given CSV file."""
+    csvfile.seek(0,2)  # Move to the last line of the file
+    while True:
+        try:
+            line = csvfile.readlines()[-1]
+        except:
+            time.sleep(0.1)   
+            continue
+        yield line
+
+def update_data_from_csv(csvfile, initial_state):
+    """
+    Append data to the lists from the CSV and send it to the 'parse_tree.py' for further processing.
+    """
+    lines = get_line_from_csv(csvfile)
+    global no_columns_csv
+    for line in lines:
+        row = line.split(',')  # Split the line by ',' as the format is CSV
+
+        if row[0] == '\n':  # Skip empty lines
+            continue
+
+        try:
+            if row[0] != 'OS_timestamp' and (len(row)-1) == no_columns_csv:
+                # Extract relevant data
+                cf.data.update({
+                    'Phase': row[2],
+                    'Hunger_AVATAR': row[11],
+                    'Health_AVATAR': row[10],
+                    'Sanity_AVATAR': row[12],
+                    'Curr_Active_Item_AVATAR': row[8],
+                    'Curr_Equip_Hands_AVATAR': row[9],
+                    'Attack_Target_AVATAR': row[13],
+                    'Defense_Target_AVATAR': row[14],
+                    'Food_AVATAR': float(row[16]),
+                    'Tool_AVATAR': row[22],
+                    'Lights_AVATAR': row[23],
+                    'Is_Fireplace_AVATAR': row[24],
+                    'Is_Light_AVATAR': row[25],
+                    'Is_Monster_AVATAR': int(row[26]),
+                    'Twigs_AVATAR': int(row[17]),
+                    'Flint_AVATAR': int(row[18]),
+                    'Rock_AVATAR': int(row[19]),
+                    'Grass_AVATAR': int(row[20]),
+                    'Log_AVATAR': int(row[21]),
+                    'Player_Xloc': row[34]
+                })
+
+                # Send the current state data to 'parse_tree.py' for processing
+                logger.info("Update state and hand over to decision tree.")
+                
+                if float(row[1]) < 60:
+                    pt.parse_decision_tree(cf.data, initial_state)
+                else:
+                    if time.time() - cf.game_start_time > 20:
+                        pt.parse_decision_tree(cf.data, initial_state)
+                    else:
+                        cf.status['Phase'].append(cf.data['Phase'])
+                        cf.status['Hunger_AVATAR'].append(int(float(cf.data['Hunger_AVATAR'])))
+                        cf.status['Health_AVATAR'].append(int(float(cf.data['Health_AVATAR'])))
+                        cf.status['Sanity_AVATAR'].append(int(float(cf.data['Sanity_AVATAR'])))
+                        cf.status['Curr_Equip_Hands_AVATAR'].append(cf.data['Curr_Equip_Hands_AVATAR'])
+                        cf.status['Attack_Target_AVATAR'].append(cf.data['Attack_Target_AVATAR'])
+                        cf.status['Defense_Target_AVATAR'].append(cf.data['Defense_Target_AVATAR'])
+                        cf.status['Food_AVATAR'].append(cf.data['Food_AVATAR'])
+                        cf.status['Twigs_AVATAR'].append(cf.data['Twigs_AVATAR'])
+                        cf.status['Flint_AVATAR'].append(cf.data['Flint_AVATAR'])
+                        cf.status['Grass_AVATAR'].append(cf.data['Grass_AVATAR'])
+                        cf.status['Log_AVATAR'].append(cf.data['Log_AVATAR'])
+                        cf.status['Twigs_AVATAR'].append(cf.data['Twigs_AVATAR'])
+                        cf.status['Rock_AVATAR'].append(cf.data['Rock_AVATAR'])
+                        cf.status['Is_Light_AVATAR'].append(cf.data['Is_Light_AVATAR'])
+                        cf.status['Is_Fireplace_AVATAR'].append(cf.data['Is_Fireplace_AVATAR'])
+                        cf.status['Is_Light_AVATAR'].append(cf.data['Is_Light_AVATAR'])
+                        cf.status['Curr_Equip_Hands_AVATAR'].append(cf.data['Curr_Equip_Hands_AVATAR'])
+                        cf.status['Is_Monster_AVATAR'].append(cf.data['Is_Monster_AVATAR'])
+                        cf.status['Player_Xloc'].append(float(cf.data['Player_Xloc']))
+            else:
+                pass
+
+        except Exception as e:
+            logger.error('Error reading current line as data: %s', e)
+            logger.error('The line is: ' + line)
+            logger.error(traceback.format_exc())
+
+def state_changed_withoutHandler():
+    """
+    Monitor the CSV file for game updates, process it to a new representation (if necessary),
+    and then execute the decision tree for the virtual avatar.
+    """
+    logger.info('Opening the CSV file and regularly checking for added lines')
+
+    # Set up empty initial state for Avatar data
+    cf.initial_state.update({
+        'Phase': '',
+        'Huger_AVATAR': '',
+        'Health_AVATAR': '',
+        'Sanity_AVATAR': '',
+        'Curr_Active_Item_AVATAR': '',
+        'Curr_Equip_Hands_AVATAR': '',
+        'Attack_Target_AVATAR': '',
+        'Defense_Target_AVATAR': '',
+        'Food_AVATAR': '',
+        'Tool_AVATAR': '',
+        'Lights_AVATAR': '',
+        'Is_Fireplace_AVATAR': '',
+        'Is_Light_AVATAR': '',
+        'Is_Monster_AVATAR': '',
+        'Twigs_AVATAR': '',
+        'Flint_AVATAR': '',
+        'Rock_AVATAR': '',
+        'Grass_AVATAR': '',
+        'Log_AVATAR': '',
+        'Player_Xloc': ''
+    })
+
+    try:
+        with open(cf.INTERFACE_FILEFOLDER, 'r', encoding='utf-8', newline='') as csvfile:
+            csvheader = csvfile.readline().strip().split(',')
+            global no_columns_csv
+            no_columns_csv = len(csvheader) - 1  # Count all columns
+
+            logger.debug(' printing all header names: ')
+
+            # Write down column information to log file
+            for i in range(no_columns_csv):
+                logger.debug('%s : %s', i, csvheader[i])
+                
+            update_data_from_csv(csvfile, cf.initial_state)
+
+    except Exception as e:
+        logger.error('Error during opening csv file: %s', e)
